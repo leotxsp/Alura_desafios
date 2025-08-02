31e7c8c0c0a5522cb0837a63cdf3bb8425be33b2
leotxsp <leotxsp40@gmail.com>
2025-08-01 21:37:39 -0300

    Desafios da terceira aula do curso logica-programacao-funcoes-listas concluidos

--- test/src/Lógica de programação explore funções e listas/desafio3.js
+++ test/src/Lógica de programação explore funções e listas/desafio3.js
@@ -25,4 +25,61 @@ function calcularFatorial(numero) {
     return fatorial;
 }
 
-calcularFatorial(5) 
+
+//3 Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+
+function calcularDolarParaReais(numero){
+    let cotacao = 4.8
+    let ValorConvertido = (numero * cotacao)
+    return ValorConvertido
+}
+//4 Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
+
+function calcularAreaEPerimetroDoRetangulo(altura,largura){
+    return{
+        perimetro: function(){
+            let p = 2*(largura+altura);
+            return p;
+        },
+        area:function(){
+            let a = largura*altura;
+            return a;
+        }
+    };
+};
+
+/*Exemplo de uso:
+alert(calcularAreaEPerimetroDoRetangulo(5,5).area())
+alert(calcularAreaEPerimetroDoRetangulo(5,5).perimetro())*/
+
+//5 Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+
+function calcularAreaEperimetroDoCirculo(raio){
+    let pi = 3.14
+    return{
+        area:function(){
+            let a = 2*pi*raio;
+            return a
+        },
+        perimetro:function(){
+            let p = pi*raio**2;
+            return p;
+        }
+    }
+}
+
+/*Exemplo de uso:
+alert(calcularAreaEperimetroDoCirculo(1).perimetro())*/
+
+//6  Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
+
+function talbada(numero){
+    for (let tabuada = 1; tabuada <= 10; tabuada++){
+        let resultado = numero*tabuada
+        console.log(`${numero} x ${tabuada} = ${resultado}`);
+    }
+}
+
+
+talbada(1)
+
