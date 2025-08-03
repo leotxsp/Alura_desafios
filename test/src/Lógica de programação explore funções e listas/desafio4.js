25c80e404306bfd9a24da5b5e494cc3499c32a3e
leotxsp <leotxsp40@gmail.com>
2025-08-02 22:22:39 -0300

    Adicionado o desafio n:4 do curso Lógica de programação: explore funções e listas

---
+++ test/src/Lógica de programação explore funções e listas/desafio4.js
@@ -0,0 +1,9 @@
+let listaGenerica = [];
+let linguagensDeProgramacao = ['Java', 'Ruby','GoLang'];
+linguagensDeProgramacao.push('JavaScript','C','C++', 'Kotlin', 'Python')
+// alert(linguagensDeProgramacao)
+
+let lista = ['Leonardo','Pedro','Bruno']
+alert(lista[0])
+alert(lista[1])
+alert(lista[2])
